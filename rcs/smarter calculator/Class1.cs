d58df6ea634485fec2ec5f77ebb284c9f18ac321
MGabrane <madara.gabrane@gmail.com>
2017-12-21 21:19:37 +0200

    created buyList

--- rcs/smarter calculator/Class1.cs
+++ rcs/smarter calculator/Class1.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace smarter_calculator
 {
-    class Class1
+    class MathParser
     {
+        //funkcija, kas saņem lietotāja ievadītu tekst
+        //saparsē un veic matemātiskās darbības
+        //atgriež rezultatu
+        public int ParsentMath(string input);
+        static void Main(string[] args)
+        {
+            //izveido kalkulatora objektu
+            MathParser parser;
+            parser = new MathParser();
+
+            //paprasīt ievadīt ievadi
+            Console.WriteLine("Please enter darbiba");
+            string input = Console.ReadLine();
+
+            //Izsauc aprēķinātu funkciju un saglabāt rezultātu
+            int result = parser.ParserMath();
+
+            //Izvada rezultatu uz ekrana
+            Console.WriteLine("your result" + result);
+            }
+            Console.ReadLine();
+        }
     }
 }
--- rcs/smarter calculator/Program.cs
+++ rcs/smarter calculator/Program.cs
@@ -15,19 +15,42 @@ namespace smarter_calculator
             //paprasīt ievadīt ievadi
             Console.WriteLine("Please enter darbiba");
             string input = Console.ReadLine();
+
+            int result = pareser.();
+
             //"1+4-5" skaits ir 5 pārējā simbola pozīcija ir 4
             //"1+" skaits ir 2, pēdēja pozīcija ir  1
             int result;
+            string FirstenteredNumber = "";
+            string SecondtenteredNumber = "";
+
+            char enteredOperation = '';
+            bool opertionFound = false;
+            
             int counter = 0;
             while (counter < input.Length) 
             {
                 char symbol = input[counter];
                 if(symbol == '+')
                 {
-                    Console.WriteLine("plus");
+                    ///Saglabā operāciju mainīgajā
+                    enteredOperation = symbol;
+                    //ielikt karodziņu, ka tad tiks pabeigts
+
+                    //ielaist nākošo skaitli, operācija ir jāizpilda
+                    opertionFound = true;
                 }
                 else
                 {
+                    if  (opertionFound == false)
+                    {
+
+                    }
+                    //saglabāt simbolu teksta virknē, lai tam
+                    //var piekļūt, kad tiek veikta darbība
+
+                    FirstenteredNumber = FirstenteredNumber + symbol;
+
                     int number;
                     number = Int32.Parse(symbol.ToString());
                     Console.WriteLine("number" + number);
