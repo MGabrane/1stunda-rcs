7a60e4a569060fa0601d3e3d38d229fd8fed3313
MGabrane <madara.gabrane@gmail.com>
2017-12-22 21:24:29 +0200

    smth about buyList

--- rcs/BuyList/MainWindow.xaml.cs
+++ rcs/BuyList/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 namespace BuyList
 {
     using System.Collections.ObjectModel;
+    using System.IO;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -29,9 +30,7 @@ namespace BuyList
         {
             InitializeComponent();
 
-            BuyListItemName.Text = "Lūdzu ievadiet pirkumu";
-            this.BuyItemList.Add("Āboli");
-            this.BuyItemList.Add("Bumbierii");
+            BuyListItemName.Text = "";
 
             //Pasakām BuyListControl, ka jāizmanto mūsu saraksts,
             //rādāmolietu avots(j askatās no saraksta, ko radīt
@@ -48,6 +47,31 @@ namespace BuyList
 
             //ierakstam iegūto vērtību teksta blokā
             this.BuyItemList.Add(enteredItemToBuy);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllLines(@"C:\Users\Madza\Desktop\Code1_buyList", this.BuyItemList);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var todosFromEile = File.ReadAllLines(@"C:\Users\Madza\Desktop\Code1_buyList");
+            for (int i = 0; 1<todosFromEile.Length; i++)
+            {
+                string curentTodo = todosFromEile[i];
+                this.BuyItemList.Add(curentTodo);
+            }
+            MessageBox.Show("all tems have been loaded");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            foreach (var selectedItem in this.BuyItemList.selectedItems)
+            {
+                this.BuyItemList.Remove(selectedItem as string);
+            }
+            
         }
     }
 }
